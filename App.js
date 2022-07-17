ba4dd253ba997c7f98bcdf67567cc9d2f0c15a0f
Joe Curtis <joecurtis9999@gmail.com>
2022-07-17 12:57:14 +0100

    file structure

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello!</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
